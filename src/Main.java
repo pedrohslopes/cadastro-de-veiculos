0851818c54b10d7d930d28ceb67941095cf5f7d5
pedrohslopes <sp.pedrolopes@gmail.com>
2023-09-14 12:34:54 -0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,91 @@
-import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Veiculo veiculo1 = new Carro("VW", "Gol", 1999, 4);
+        Veiculo veiculo2 = new Carro("GM", "Opala", 1964, 2);
+        Veiculo veiculo3 = new Moto("Honda", "Cg", 2021, "Esportiva");
 
-public class Main { public static void main(String[] args) {
-    System.out.println("=====Bem Vindo ao Sistema de veiculos=====\r\n" +
-            "digite os dados do veiculo para começar:\r\n" +
-            "1. Cadastrar veiculos\r\n");
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Primeiro, digite o nome do veiculo:");
-    var nome = scanner.nextLine();
-    System.out.println("Agora, digite a marca do veiculo: ");
-    var marca = scanner.nextLine();
-    System.out.println("Digite o modelo do veiculo: ");
-    var modelo = scanner.nextLine();
-    System.out.println("Agora, digite o ano de fabricaçao:");
-    var fabricacao = scanner.nextInt();
+        veiculo1.acelerar();
+        if (veiculo1 instanceof Carro) {
+            ((Carro) veiculo1).abrirPorta();
+        }
 
-    var cadastrado = true;
-    System.out.println(" Nome: "+ nome + "\r\n, Marca:" + marca + "\r\n,Modelo: " + modelo +  "\r\n,Ano de fabricaçao: " + fabricacao + "\r\n,Ele esta cadastrado: " + cadastrado);
+        veiculo2.acelerar();
+        if (veiculo2 instanceof Carro) {
+            ((Carro) veiculo2).abrirPorta();
+        }
+
+        veiculo3.acelerar();
+        if (veiculo3 instanceof Moto) {
+            ((Moto) veiculo3).empinar();
+        }
+    }
 }
+class Veiculo {
+    private String marca;
+    private String modelo;
+    private int ano;
+
+    public Veiculo(String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    public void acelerar() {
+        System.out.println("O " + modelo + " está acelerando.");
+    }
+
+    public void frear() {
+        System.out.println("O " + modelo + " está freando.");
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
 }
+
+class Carro extends Veiculo {
+    private int quantidadePortas;
+
+    public Carro(String marca, String modelo, int ano, int quantidadePortas) {
+        super(marca, modelo, ano);
+        this.quantidadePortas = quantidadePortas;
+    }
+
+    @Override
+    public void acelerar() {
+        System.out.println("O carro " + getModelo() + " está acelerando.");
+    }
+
+    public void abrirPorta() {
+        System.out.println("Abrindo " + quantidadePortas + " portas do " + getModelo() + ".");
+    }
+}
+
+class Moto extends Veiculo {
+    private String tipo;
+
+    public Moto(String marca, String modelo, int ano, String tipo) {
+        super(marca, modelo, ano);
+        this.tipo = tipo;
+    }
+
+    @Override
+    public void acelerar() {
+        System.out.println("A moto " + getModelo() + " está acelerando.");
+    }
+
+    public void empinar() {
+        System.out.println("A moto " + getModelo() + " do tipo " + tipo + " está empinando.");
+    }
+}
+
+
